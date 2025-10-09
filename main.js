071361e9abad743dc62f1a869ea6baef50aa73b0
Kellocx <marialessandro79@gmail.com>
2025-10-09 09:04:02 +0200

    header

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
-const carousel = new bootstrap.Carousel('#carouselTesla')
+//const carousel = new bootstrap.Carousel('#carouselTesla')
 const carouselTeslaElement = document.querySelector('#carouselTesla')
 
 const carousel = new bootstrap.Carousel(carouselTeslaElement, {
   interval: 2000,
   touch: false
-})
+});
